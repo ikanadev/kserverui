1e790121d3a74d849958c24c2e1547c3577ec44a
Kevin Vargas <vargaskevv@gmail.com>
2025-01-29 01:14:01 -0400

    show battery, cpu and ram usage in index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,60 @@
+import { Show } from "solid-js";
 import { Title } from "@solidjs/meta";
-import Counter from "~/components/Counter";
+import { createAsync, query } from "@solidjs/router";
+import styles from "./index.module.css";
+
+type ServerData = {
+	battery: number;
+	ram: {
+		total: number;
+		used: number;
+	};
+	cpu: number;
+}
+const getServerData = query(async function(): Promise<ServerData | null> {
+	"use server";
+	try {
+		const batteryStr = await Bun.$`cat /sys/class/power_supply/BAT0/capacity`.text();
+		// const batteryStr = await Bun.$`cat /sys/class/net/enp6s0/statistics/rx_packets`.text();
+		const battery = parseInt(batteryStr.trim());
+
+		const ramStr = await Bun.$`free -m | awk '/Mem:/ {printf "%s %s", $2, $3}'`.text();
+		const ramSections = ramStr.trim().split(" ");
+
+		const cpuStr = await Bun.$`grep 'cpu ' /proc/stat | awk '{idle=$5; total=$2+$3+$4+$5; print (1-idle/total)*100}'`.text();
+		const cpu = parseFloat(cpuStr.trim());
+		return {
+			battery,
+			ram: {
+				total: parseInt(ramSections[0]),
+				used: parseInt(ramSections[1]),
+			},
+			cpu,
+		};
+	} catch (e) {
+		console.log("Error:", e);
+		return null;
+	}
+}, "serverData");
+
+export const route = {
+	preload: () => getServerData(),
+};
 
 export default function Home() {
-  return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
-  );
+	const serverData = createAsync(() => getServerData());
+	return (
+		<main class={styles.container}>
+			<Title>Server Status</Title>
+			<h1>Server Status</h1>
+			<Show when={serverData?.()}>{(data) => (
+				<div>
+					<p>Battery: {data().battery}%</p>
+					<p>RAM: {Math.round(data().ram.used / data().ram.total * 100)}%  -  {(data().ram.used/1024).toFixed(2)}/{(data().ram.total/1024).toFixed(2)}MB</p>
+					<p>CPU: {data().cpu.toFixed(2)}%</p>
+				</div>
+			)}
+			</Show>
+		</main>
+	);
 }
