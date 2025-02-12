c861cd565be7c4e76cf8d7ff137c103439083ece
Kevin Vargas <vargaskevv@gmail.com>
2025-02-11 23:18:56 -0400

    test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,37 +1,56 @@
 import { Show } from "solid-js";
 import { Title } from "@solidjs/meta";
-import { createAsync, query } from "@solidjs/router";
+import { createAsync, query, action } from "@solidjs/router";
+
 import { getServerStatus } from "~/utils/server";
 import styles from "./index.module.css";
 import { ServerStatus } from "~/utils/types";
 
-const serverStatus = query(async function(): Promise<ServerStatus> {
-	"use server";
-	const data = await getServerStatus();
-	return data;
+const sendMessage = action(async (msg: string) => {
+  "use server";
+  console.log("Sending message:", msg);
+});
+
+
+const serverStatus = query(async function(): Promise<any> {
+  "use server";
+  try {
+    const data = await getServerStatus();
+    const resp = await Bun.fetch('http://host.docker.internal:3080');
+    const respText = await resp.text();
+    console.log(respText);
+    return { data, respText };
+  } catch (error) {
+    console.error(error);
+    return { error };
+  }
 }, "serverStatus");
 
 export const route = {
-	preload: () => serverStatus(),
+  preload: () => serverStatus(),
 };
 
 export default function Home() {
-	const status = createAsync(() => serverStatus());
-	return (
-		<main class={styles.container}>
-			<Title>Server Status</Title>
-			<h1>Server Status</h1>
-			<Show when={status?.()}>{(data) => (
-				<div>
-					<pre>{JSON.stringify(data(), null, 2)}</pre>
-					{/*
+  const status = createAsync(() => serverStatus());
+  return (
+    <main class={styles.container}>
+      <Title>Server Status</Title>
+      <h1>Server Status</h1>
+      <div>
+        <button onClick={() => { sendMessage("test"); }}>Send Message</button>
+      </div>
+      <Show when={status?.()}>{(data) => (
+        <div>
+          <img src="/gopher.svg" alt="kserverui logo" />
+          <pre>{JSON.stringify(data(), null, 2)}</pre>
+          {/*
 					<p>Battery: {data().battery}%</p>
 					<p>RAM: {Math.round(data().ram.used / data().ram.total * 100)}%  -  {(data().ram.used/1024).toFixed(2)}/{(data().ram.total/1024).toFixed(2)}MB</p>
 					<p>CPU: {data().cpu.toFixed(2)}%</p>
 					*/}
-				</div>
-			)}
-			</Show>
-		</main>
-	);
+        </div>
+      )}
+      </Show>
+    </main>
+  );
 }
