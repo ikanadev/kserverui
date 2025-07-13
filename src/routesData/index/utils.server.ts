1506286b4852c2e0d2107459fad823830ea9841e
Kevin Vargas <vargaskevv@gmail.com>
2025-07-12 23:52:40 -0400

    remove old app and start new svelte kit base template

--- src/routesData/index/utils.server.ts
+++
@@ -1,8 +0,0 @@
-import type { ServerStatus } from "@app/utils/types";
-
-export async function fetchServerStatus() {
-  "use server";
-  const resp = await Bun.fetch(`${process.env.HOME_PATH}/stats`);
-  const data = (await resp.json()) as ServerStatus;
-  return data;
-}
